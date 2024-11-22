836955b58b4f6741f4527064b2ac7c072dd12c6c
nouran <nouranessamy@gmail.com>
2024-11-22 18:20:10 +0200

    Update QueueDerived.h

--- QueueDerived.h
+++ QueueDerived.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Project allowed DS/LinkedQueue.h"
+
+template<typename T>
+class QueueDerived : public  LinkedQueue<T> {
+public:
+	QueueDerived() :LinkedQueue() {
+
+	}
+	bool Cancel(int ID) {
+		//test
+	}
+};
